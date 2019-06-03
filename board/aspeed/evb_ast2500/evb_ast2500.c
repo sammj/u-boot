a1a0b3c12239a793edc1e33a7d382048c7408286
Joel Stanley <joel@jms.id.au>
2019-06-03 16:24:03 +0200

    aspeed: Unlock SCU before jumping to Linux
    Linux assumes the SCU is unlocked. This is common across all of the
    ASPEED systems but the board callback was the first I could find.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- board/aspeed/evb_ast2500/evb_ast2500.c
+++ board/aspeed/evb_ast2500/evb_ast2500.c
@@ -3,3 +3,17 @@
  * Copyright (c) 2016 Google, Inc
  */
 #include <common.h>
+
+#include <asm/arch/scu_ast2500.h>
+#include <linux/err.h>
+
+void board_quiesce_devices(void)
+{
+	struct ast2500_scu *scu = ast_get_scu();
+	if (IS_ERR(scu)) {
+		printf("Could not unlock SCU, Linux will not work\n");
+		return;
+	}
+
+	ast_scu_unlock(scu);
+}
