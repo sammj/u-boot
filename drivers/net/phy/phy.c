94d6c44e6d324073b35c80a637f3b670121abfa8
Samuel Mendoza-Jonas <sam@mendozajonas.com>
2019-06-04 13:20:06 +1000

    phy: Include NC-SI in phy setup
    Add NC-SI to the usual phy handling. This makes two notable changes:
    - Somewhat similar to a fixed phy, phy_connect() will create an NC-SI
    phy if CONFIG_PHY_NCSI is defined.
    - An early return is added to phy_read() and phy_write() to handle a
    case like the NC-SI phy which does not define a bus.
    
    Signed-off-by: Samuel Mendoza-Jonas <sam@mendozajonas.com>

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -540,6 +540,9 @@ int phy_init(void)
 #ifdef CONFIG_PHY_FIXED
 	phy_fixed_init();
 #endif
+#ifdef CONFIG_PHY_NCSI
+	phy_ncsi_init();
+#endif
 	return 0;
 }
 
@@ -945,6 +948,10 @@ struct phy_device *phy_connect(struct mii_dev *bus, int addr,
 
 #ifdef CONFIG_PHY_FIXED
 	phydev = phy_connect_fixed(bus, dev, interface);
+#endif
+
+#ifdef CONFIG_PHY_NCSI
+	phydev = phy_device_create(bus, 0, PHY_NCSI_ID, false, interface);
 #endif
 
 	if (!phydev)
